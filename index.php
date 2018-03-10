60cf81d20c2bd18e76719a4f5bc7ce74c4588c8e
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-03-10 14:53:00 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Project Portunus</title>
+		<link rel=icon href=/images/favicon-16x16.png sizes="16x16" type="image/png">
+		<link rel=icon href=/images/favicon-32x32.png sizes="32x32" type="image/png">
+		<link rel=icon href=/images/favicon-96x96.png sizes="96x96" type="image/png">
+		<meta charset="UTF-8">
+		<meta name="description" content="Webpage access point for uploading and parsing archives for Hanover Town Hall">
+		<meta name="keywords" content="hanover,new,hampshire,portunus,digitization">
+		<meta name="author" content="Maisy Wilcox">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="/css/theme.css">
+	</head>
+	
+	<header>
+		<nav>
+		</nav>
+	</header>
+	
+	<body>
 
+
+	</body>
+	
+	<footer>
+		<p>All content licensed under <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPLv3</a> | Contact me at <a href="mailto:maisy.wilcox@maisywilcox.com">
+		maisy.wilcox@maisywilcox.com</a></p>
+	</footer>
+</html>
